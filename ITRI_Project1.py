ee824e0917020f15f6407981bf9b46ff70d3b7d1
benlin131020 <1104105332@gm.kuas.edu.tw>
2018-07-24 16:48:59 +0800

    __init__

---
+++ ITRI_Project1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+
+def roi(img):
+    #sharpen
+    kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    sharpen = cv2.filter2D(img, -1, kernel)
+    #gaussianblur
+    blur = cv2.GaussianBlur(sharpen,(5,5),0)
+    #otsu threshold
+    ret2,th2 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #closing
+    kernel = np.ones((5,5),np.uint8)
+    closing = cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel)
+    #contuour
+    image, contours, hierarchy = cv2.findContours(closing,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    img=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+    roi = cv2.drawContours(img, contours, -1, (0,0,255), 1)
+    return roi
+
+#main
+img1=cv2.imread("37736735_2061362360549587_9024993033665380352_n.jpg",0)
+img2=cv2.imread("37770404_2061362363882920_3525037766163300352_n.jpg",0)
+img3=cv2.imread("37673579_2061362367216253_6417462665358082048_n.jpg",0)
+
+roi1=roi(img1)
+roi2=roi(img2)
+roi3=roi(img3)
+#stacking images side-by-side
+img1=cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
+img2=cv2.cvtColor(img2,cv2.COLOR_GRAY2BGR)
+img3=cv2.cvtColor(img3,cv2.COLOR_GRAY2BGR)
+res1=np.hstack((img1,roi1))
+res2=np.hstack((img2,roi2))
+res3=np.hstack((img3,roi3))
+cv2.imshow("result1",res1)
+cv2.imshow("result2",res2)
+cv2.imshow("result3",res3)
+
+
+
+
+cv2.waitKey(0)
